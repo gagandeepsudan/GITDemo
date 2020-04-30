f80a839ae2d34940750f9c3b821d1fdfbae51548
gagandeepsudan <gagandeepsudan@hotmail.com>
2020-04-30 21:05:33 +0100

    New Commit

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -9,7 +9,12 @@ public class RESTAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
-		
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
+		System.out.println("postJira8");
+		System.out.println("postJira9");		
 	}
 	
 	@Test
